4cb5ddf2aebc2f0c79505f8b62078cc01b092275
zygnematophyce <lfourteau91@gmail.com>
2022-12-02 15:18:49 +0100

    Part 2.

--- day_2/js/script.js
+++ day_2/js/script.js
@@ -3,6 +3,7 @@ console.log('--- Day 2: Rock Paper Scissors ---');
 const contentTextArea = document.querySelector('#rock-paper-scissors');
 const btn = document.querySelector('.btn');
 const spanTotal = document.querySelector('.total-score');
+const spanSecret = document.querySelector('.secret-score');
 
 function getRockPaperScissorsMatrice(rounds) {
   const round = rounds.trim().split('\n');
@@ -44,9 +45,46 @@ function getMaxScore(game) {
   return totalScore;
 }
 
+function getMaxSecretScrore(game) {
+  let totalScore = 0;
+  for (let i = 0; i < game.length; i++) {
+    let choicePlayerOne = game[i][0];
+    let choicePlayerTwo = game[i][1];
+    if (choicePlayerOne === 'A') {
+      if (choicePlayerTwo === 'X') {
+        totalScore += 3;
+      } else if (choicePlayerTwo === 'Y') {
+        totalScore += 4;
+      } else {
+        totalScore += 8;
+      }
+    } else if (choicePlayerOne === "B") {
+      if (choicePlayerTwo === 'X') {
+        totalScore += 1;
+      } else if (choicePlayerTwo === 'Y') {
+        totalScore += 5;
+      } else {
+        totalScore += 9;
+      }
+    } else {
+      if (choicePlayerTwo === 'X') {
+        totalScore += 2;
+      } else if (choicePlayerTwo === 'Y') {
+        totalScore += 6;
+      } else {
+        totalScore += 7;
+      }
+    }
+  }
+  return totalScore;
+}
+
 btn.addEventListener('click', () => {
   const values = contentTextArea.value;
   const eachGameMatrice = getRockPaperScissorsMatrice(values);
   const total = getMaxScore(eachGameMatrice);
   spanTotal.textContent = total;
+
+  const secretTotal = getMaxSecretScrore(eachGameMatrice);
+  spanSecret.textContent = secretTotal;
 });
