963c20cca3028c98d221238effa772f3a0cbe68e
zygnematophyce <lfourteau91@gmail.com>
2022-12-08 20:02:18 +0100

    Day 4 part 1 done.

---
+++ day_4/js/script.js
@@ -0,0 +1,28 @@
+const contentArea = document.querySelector('#input');
+const btn = document.querySelector('.btn');
+const span1 = document.querySelector('.total-contain');
+
+function countRangeContain(mtx) {
+  let count = 0;
+  for (let i = 0; i < mtx.length; i++) {
+    if ((mtx[i][0] >= mtx[i][2] && mtx[i][1] <= mtx[i][3]) || (mtx[i][0] <= mtx[i][2] && mtx[i][1] >= mtx[i][3])) {
+      console.log(mtx[i]);
+      count += 1;
+    }
+  }
+  return count;
+}
+
+function getMatrixValues(str) {
+  let lines = str.trim().split('\n')
+  return mtx = lines.map((line) => {
+    let array = line.split(',').map((a) => a.split('-'));
+    return [Number(array[0][0]), Number(array[0][1]), Number(array[1][0]), Number(array[1][1])];
+  });
+}
+
+btn.addEventListener('click', () => {
+  const matrix = getMatrixValues(contentArea.value);
+  const countContain = countRangeContain(matrix);
+  span1.textContent = countContain;
+});
