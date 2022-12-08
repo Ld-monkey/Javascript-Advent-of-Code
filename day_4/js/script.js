a15c9aa261e0c503ebb0bf9887c224ec8a4aee3e
zygnematophyce <lfourteau91@gmail.com>
2022-12-08 20:50:05 +0100

    Day 4 part 2 done.

--- day_4/js/script.js
+++ day_4/js/script.js
@@ -1,12 +1,25 @@
 const contentArea = document.querySelector('#input');
 const btn = document.querySelector('.btn');
 const span1 = document.querySelector('.total-contain');
+const span2 = document.querySelector('.total-overlap');
 
 function countRangeContain(mtx) {
   let count = 0;
   for (let i = 0; i < mtx.length; i++) {
     if ((mtx[i][0] >= mtx[i][2] && mtx[i][1] <= mtx[i][3]) || (mtx[i][0] <= mtx[i][2] && mtx[i][1] >= mtx[i][3])) {
-      console.log(mtx[i]);
+      count += 1;
+    }
+  }
+  return count;
+}
+
+function countOverlap(mtx) {
+  let count = 0;
+  for (let i = 0; i < mtx.length; i++) {
+    if ((mtx[i][0] >= mtx[i][2] && mtx[i][0] <= mtx[i][3]) ||
+        (mtx[i][1] >= mtx[i][2] && mtx[i][1] <= mtx[i][3]) ||
+        (mtx[i][2] >= mtx[i][0] && mtx[i][2] <= mtx[i][1]) ||
+        (mtx[i][3] >= mtx[i][0] && mtx[i][3] <= mtx[i][1])) {
       count += 1;
     }
   }
@@ -23,6 +36,12 @@ function getMatrixValues(str) {
 
 btn.addEventListener('click', () => {
   const matrix = getMatrixValues(contentArea.value);
+
+  /** Part 1. */
   const countContain = countRangeContain(matrix);
   span1.textContent = countContain;
+
+  /** Part 2. */
+  const overlap = countOverlap(matrix);
+  span2.textContent = overlap;
 });
